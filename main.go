9b770fde219ba63e14f7ea8357acd987417cdaf6
Juan Manuel <jmanuelabanto@gmail.com>
2022-03-31 17:33:36 -0500

    Add files via upload

---
+++ main.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	_ "github.com/juanmaabanto/go-ms-beers/docs"
+
+	"github.com/joho/godotenv"
+	"github.com/juanmaabanto/go-ms-beers/common/managers"
+	"github.com/juanmaabanto/go-ms-beers/common/middleware"
+	"github.com/juanmaabanto/go-ms-beers/internal/ports"
+	"github.com/juanmaabanto/go-ms-beers/internal/service"
+	"github.com/juanmaabanto/go-ms-beers/internal/validations"
+	"github.com/labstack/echo/v4"
+	echoSwagger "github.com/swaggo/echo-swagger"
+)
+
+// @title Beer API
+// @version v1
+// @description Specifying services for falabella.
+
+// @contact.name Juan Manuel Abanto Mera
+// @contact.url https://www.linkedin.com/in/juanmanuelabanto/
+// @contact.email jmanuelabanto@gmail.com
+
+// @license.name MIT License
+// @license.url http://www.apache.org/licenses/LICENSE-2.0.html
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	router := echo.New()
+	ctx := context.Background()
+
+	application := service.NewApplication(ctx)
+
+	Handler(ports.NewHttpServer(application), router)
+	router.Logger.Fatal(router.Start(":3000"))
+}
+
+type ServerInterface interface {
+	AddBeer(c echo.Context) error
+	GetBeer(c echo.Context) error
+	ListBeer(c echo.Context) error
+	GetBoxPrice(c echo.Context) error
+}
+
+func Handler(si ServerInterface, router *echo.Echo) {
+	if router == nil {
+		router = echo.New()
+	}
+
+	router.Validator = validations.NewValidationUtil()
+
+	loggerManager := managers.NewLoggerManager("https://logger.mydominio.pe/", "ms-beer")
+
+	api := router.Group("/beers")
+
+	api.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		LoggerErrorFunc: loggerManager.Error,
+	}))
+
+	//Swagger
+	router.GET("/*", echoSwagger.WrapHandler)
+
+	//beer
+	api.GET("", si.ListBeer)
+	api.GET("/:beerId", si.GetBeer)
+	api.POST("", si.AddBeer)
+	api.GET("/:beerId/boxprice", si.GetBoxPrice)
+}
